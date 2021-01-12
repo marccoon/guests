b82a561b1aab211a1bb16dd04ede35e3ebafc503
Evillgranny <tractor2554393@yandex.by>
2021-01-13 02:42:08 +0300

    init

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Header  />
+
+    <Nuxt />
+
+    <Footer />
+  </div>
+</template>
+<style>
+
+</style>
+<script>
+import Header from "~/components/header/Header";
+import Footer from "~/components/footer/Footer";
+export default {
+  components: {Footer, Header}
+}
+</script>
